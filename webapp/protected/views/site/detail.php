cf136ffed81ee66e8a4d4e30b5ac358e20b77a43
anggri2013 <anggriawan@lum3u.com>
2013-10-20 23:12:21 +0700

    anggri sync

--- webapp/protected/views/site/detail.php
+++ webapp/protected/views/site/detail.php
@@ -1,3 +1,7 @@
 <?php
 echo $model['title'];
+
 ?>
+<br/><br/>
+
+Images(<?php echo $model['image'] ?>) Videos(<?php echo $model['video'] ?>) Promos(<?php echo $model['promo'] ?>) Events(<?php echo $model['event'] ?>) Product(<?php echo $model['product'] ?>) Documents(<?php echo $model['document'] ?>) News(<?php echo $model['new'] ?>) Article(<?php echo $model['article'] ?>)
